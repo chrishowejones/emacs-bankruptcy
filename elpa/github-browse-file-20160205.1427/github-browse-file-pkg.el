9d1992ee3dd901e33a6acdbed1012ac781baca73
Chris Howe-Jones <chris.howejones@gmail.com>
2024-07-01 12:15:51 +0100

    Added some more directory, movement, navigation stuff.
    Also added in some minor modes for git and magit.

---
+++ elpa/github-browse-file-20160205.1427/github-browse-file-pkg.el
@@ -0,0 +1,14 @@
+(define-package "github-browse-file" "20160205.1427" "View the file you're editing on GitHub"
+  '((cl-lib "0.5"))
+  :commit "177667b8dac640f3dabacc4395e09451c5e88c53" :authors
+  '(("Ozan Sener" . "ozan@ozansener.com"))
+  :maintainers
+  '(("Ozan Sener" . "ozan@ozansener.com"))
+  :maintainer
+  '("Ozan Sener" . "ozan@ozansener.com")
+  :keywords
+  '("convenience" "vc" "git" "github")
+  :url "https://github.com/osener/github-browse-file")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- local/custom.el
+++ local/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(perspective projectile marginalia which-key consult orderless buffer-move git-timemachine git-link git-gutter-fringe+ magit company all-the-icons default-text-scale vscode-dark-plus-theme window-number key-chord loccur multiple-cursors diminish vertico auto-compile)))
+   '(crux git-messenger github-browse-file treemacs-icons-dired treemacs-projectile treemacs-magit treemacs paredit markdown-mode perspective projectile marginalia which-key consult orderless buffer-move git-timemachine git-link git-gutter-fringe+ magit company all-the-icons default-text-scale vscode-dark-plus-theme window-number key-chord loccur multiple-cursors diminish vertico auto-compile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
