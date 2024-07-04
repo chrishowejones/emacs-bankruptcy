4191fc46fcffda0d43d4d3ecb1974319a146452f
Chris Howe-Jones <chrishowe-jones@F124236Y7M.local>
2024-07-04 12:02:19 +0100

    Add Org scheduling, agenda and basic prog mode defaults

---
+++ elpa/redtick-20180424.2136/redtick-pkg.el
@@ -0,0 +1,14 @@
+(define-package "redtick" "20180424.2136" "Smallest pomodoro timer (1 char)"
+  '((emacs "24.4"))
+  :commit "0faa6b7b479fae39f5d4632f0cbbef0f2917780e" :authors
+  '(("F. Febles"))
+  :maintainers
+  '(("F. Febles"))
+  :maintainer
+  '("F. Febles")
+  :keywords
+  '("calendar")
+  :url "http://github.com/ferfebles/redtick")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
