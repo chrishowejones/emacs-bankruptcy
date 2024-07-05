030bcbc4630d150b9b60715259bf68932d81fab4
Chris Howe-Jones <chris.howejones@gmail.com>
2024-07-05 16:38:20 +0100

    Added command log, winnow, ag, rg and wgrep modes.

---
+++ elpa/winnow-20210105.1919/winnow-pkg.el
@@ -0,0 +1,14 @@
+(define-package "winnow" "20210105.1919" "winnow ag/grep results by matching/excluding lines"
+  '((emacs "24"))
+  :commit "c3beff15688481162d14ae8600f59a366bb4c829" :authors
+  '(("Charles L.G. Comstock" . "dgtized@gmail.com"))
+  :maintainers
+  '(("Charles L.G. Comstock" . "dgtized@gmail.com"))
+  :maintainer
+  '("Charles L.G. Comstock" . "dgtized@gmail.com")
+  :keywords
+  '("matching")
+  :url "https://github.com/dgtized/winnow.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
