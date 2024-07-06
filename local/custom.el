88d2a0438e7b8ddba51a28be456f5543707ff3f7
Chris Howe-Jones <chris.howejones@gmail.com>
2024-07-06 11:27:16 +0100

    Fixed M-: (eval) to use consult-completion-in-region

--- local/custom.el
+++ local/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(completion-category-overrides nil nil nil "Customized with use-package orderless")
  '(package-selected-packages
    '(command-log-mode winnow rg ag consult-projectile smartscan js-react-redux-yasnippets java-snippets yasnippet-snippets yasnippet highlight-symbol rainbow-mode flycheck rainbow-delimiters redtick org-pomodoro org-gcal dashboard yaml-mode restclient-jq restclient ob-http org-bullets org-auto-tangle ox-reveal orgit org-magit crux git-messenger github-browse-file treemacs-icons-dired treemacs-projectile treemacs-magit treemacs paredit markdown-mode perspective projectile marginalia which-key consult orderless buffer-move git-timemachine git-link git-gutter-fringe+ magit company all-the-icons default-text-scale vscode-dark-plus-theme window-number key-chord loccur multiple-cursors diminish vertico auto-compile)))
 (custom-set-faces
