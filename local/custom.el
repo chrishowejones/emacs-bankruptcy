426540634fd7c6b4da4c124b3501f6b39aa67ca6
Chris Howe-Jones <chris.howejones@gmail.com>
2024-07-21 09:17:50 +0100

    Custom.el reset.

--- local/custom.el
+++ local/custom.el
@@ -5,11 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(command-log-mode-key-binding-open-log [67 45 99 32 108])
  '(package-selected-packages
-   '(vc-use-package use-package conventional-commit jest-test-mode typescript-mode apheleia gist ts-comint tree-sitter-langs tree-sitter js2-mode consult-lsp lsp-ui lsp-mode flycheck-joker flycheck-clj-kondo flycheck-pos-tip flycheck-clojure cljsbuild-mode clj-refactor cider bash-completion command-log-mode winnow rg ag consult-projectile smartscan js-react-redux-yasnippets java-snippets yasnippet-snippets yasnippet highlight-symbol rainbow-mode flycheck rainbow-delimiters redtick org-pomodoro org-gcal dashboard yaml-mode restclient-jq restclient ob-http org-bullets org-auto-tangle ox-reveal orgit org-magit crux git-messenger github-browse-file treemacs-icons-dired treemacs-projectile treemacs-magit treemacs paredit markdown-mode perspective projectile marginalia which-key consult orderless buffer-move git-timemachine git-link git-gutter-fringe+ magit company all-the-icons default-text-scale vscode-dark-plus-theme window-number key-chord loccur multiple-cursors diminish vertico auto-compile))
+   '(conventional-commit vc-use-package persp-projectile embark-consult embark tree-sitter-langs tree-sitter js2-mode consult-lsp lsp-ui lsp-mode flycheck-joker flycheck-clj-kondo flycheck-pos-tip flycheck-clojure cljsbuild-mode clj-refactor cider bash-completion command-log-mode winnow rg ag consult-projectile smartscan js-react-redux-yasnippets java-snippets yasnippet-snippets yasnippet highlight-symbol rainbow-mode flycheck rainbow-delimiters redtick org-pomodoro org-gcal dashboard yaml-mode restclient-jq restclient ob-http org-bullets org-auto-tangle ox-reveal orgit org-magit crux git-messenger github-browse-file treemacs-icons-dired treemacs-projectile treemacs-magit treemacs paredit markdown-mode perspective projectile marginalia which-key consult orderless buffer-move git-timemachine git-link git-gutter-fringe+ magit company all-the-icons default-text-scale vscode-dark-plus-theme window-number key-chord loccur multiple-cursors diminish vertico auto-compile))
  '(package-vc-selected-packages
    '((conventional-commit :url "https://github.com/akirak/conventional-commit.el.git" :branch "master")
-     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package")))
- '(safe-local-variable-values '((eval setq projectile-project-test-suffix ".spec"))))
+     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
