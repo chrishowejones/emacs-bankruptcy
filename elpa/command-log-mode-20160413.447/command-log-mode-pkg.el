e685980643a9ad8be6af19745373dc1dd973d8c9
Chris Howe-Jones <chris.howejones@gmail.com>
2024-10-20 09:50:09 +0100

    Use smartparens and update command log mode.

--- elpa/command-log-mode-20160413.447/command-log-mode-pkg.el
+++ elpa/command-log-mode-20160413.447/command-log-mode-pkg.el
@@ -1,12 +1,10 @@
-(define-package "command-log-mode" "20160413.447" "log keyboard commands to buffer" 'nil :commit "af600e6b4129c8115f464af576505ea8e789db27" :authors
-  '(("Michael Weber" . "michaelw@foldr.org"))
-  :maintainers
-  '(("Michael Weber" . "michaelw@foldr.org"))
-  :maintainer
-  '("Michael Weber" . "michaelw@foldr.org")
-  :keywords
-  '("help")
-  :url "https://github.com/lewang/command-log-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "command-log-mode" "20160413.447"
+  "Log keyboard commands to buffer."
+  ()
+  :url "https://github.com/lewang/command-log-mode"
+  :commit "af600e6b4129c8115f464af576505ea8e789db27"
+  :revdesc "af600e6b4129"
+  :keywords '("help")
+  :authors '(("Michael Weber" . "michaelw@foldr.org"))
+  :maintainers '(("Michael Weber" . "michaelw@foldr.org")))
