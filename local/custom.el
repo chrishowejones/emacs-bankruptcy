ad79fef5addae5c7f1589b757bf42a72053d3035
Chris Howe-Jones <chris.howejones@gmail.com>
2024-10-20 10:41:49 +0100

    Update command mode log key binding.

--- local/custom.el
+++ local/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(command-log-mode-key-binding-open-log [67 45 99 32 108])
  '(package-selected-packages
    '(editorconfig web-mode code-review forge keycast dockerfile-mode dockerfile github-review-mode github-review terraform-mode terraform conventional-commit vc-use-package persp-projectile embark-consult embark tree-sitter-langs tree-sitter js2-mode consult-lsp lsp-ui lsp-mode flycheck-joker flycheck-clj-kondo flycheck-pos-tip flycheck-clojure cljsbuild-mode clj-refactor cider bash-completion command-log-mode winnow rg ag consult-projectile smartscan js-react-redux-yasnippets java-snippets yasnippet-snippets yasnippet highlight-symbol rainbow-mode flycheck rainbow-delimiters redtick org-pomodoro org-gcal dashboard yaml-mode restclient-jq restclient ob-http org-bullets org-auto-tangle ox-reveal orgit org-magit crux git-messenger github-browse-file treemacs-icons-dired treemacs-projectile treemacs-magit treemacs paredit markdown-mode perspective projectile marginalia which-key consult orderless buffer-move git-timemachine git-link git-gutter-fringe+ magit company all-the-icons default-text-scale vscode-dark-plus-theme window-number key-chord loccur multiple-cursors diminish vertico auto-compile))
  '(package-vc-selected-packages
