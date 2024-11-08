5b3836b1e1fc6c12e1a2f7c55c5d08f9b1172077
Chris Howe-Jones <chrishowe-jones@F124236Y7M.local>
2024-11-08 13:00:55 +0000

    Updated use of tree sitter.

---
+++ elpa/tree-sitter-langs-20241027.912/tree-sitter-langs-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "tree-sitter-langs" "20241027.912"
+  "Grammar bundle for tree-sitter."
+  '((emacs       "25.1")
+    (tree-sitter "0.15.0"))
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs"
+  :commit "562e4681d365c70169d807e23db4c37427628627"
+  :revdesc "562e4681d365"
+  :keywords '("languages" "tools" "parsers" "tree-sitter")
+  :authors '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainers '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")))
