4e87ed35d8cc5d835819bc0ee3b70db414ea96e8
Chris Howe-Jones <chris.howejones@gmail.com>
2025-02-22 15:37:09 +0000

    Add Erlnag goodness and tidy up whitespaces and trailing new lines.

---
+++ elpa/company-distel-20180827.1344/company-distel-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "company-distel" "20180827.1344"
+  "Erlang/distel completion backend for company-mode."
+  '((distel-completion-lib "1.0.0"))
+  :url "github.com/sebastiw/distel-completion"
+  :commit "acc4c0a5521904203d797fe96b08e5fae4233c7e"
+  :revdesc "acc4c0a55219"
+  :keywords '("erlang" "distel" "company"))
---
+++ elpa/distel-completion-lib-20180827.1344/distel-completion-lib-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "distel-completion-lib" "20180827.1344"
+  "Completion library for Erlang/Distel."
+  ()
+  :url "github.com/sebastiw/distel-completion"
+  :commit "acc4c0a5521904203d797fe96b08e5fae4233c7e"
+  :revdesc "acc4c0a55219"
+  :keywords '("erlang" "distel" "completion"))
