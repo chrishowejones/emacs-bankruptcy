e63284f9346f68ecd6bf20120ed7c2adc36dfe7b
Chris Howe-Jones <chris.howejones@gmail.com>
2025-03-10 16:59:23 +0000

    More updates.

---
+++ elpa/github-dark-vscode-theme-0.4.0/github-dark-vscode-theme-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "github-dark-vscode-theme" "0.4.0"
+  "The GitHub Dark Theme from Visual Studio Code."
+  '((emacs "25.1"))
+  :url "https://github.com/justintime50/github-dark-vscode-emacs-theme"
+  :commit "00cac57857732999681e14d0c04fd8b8dbf3ef2d"
+  :revdesc "00cac5785773"
+  :keywords '("faces"))
---
+++ elpa/vscode-dark-plus-theme-2.0.0/vscode-dark-plus-theme-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "vscode-dark-plus-theme" "2.0.0"
+  "Default Visual Studio Code Dark+ theme."
+  ()
+  :url "https://github.com/ianpan870102/vscode-dark-plus-emacs-theme"
+  :commit "41772165b3b1195a7e86747ea5b316b16be4c7ef"
+  :revdesc "41772165b3b1")
