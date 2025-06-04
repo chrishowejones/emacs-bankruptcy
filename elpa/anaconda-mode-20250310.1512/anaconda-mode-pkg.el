7c7124de344e5d187191481dc4d6070596eaddd6
Chris Howe-Jones <chris.howejones@gmail.com>
2025-06-04 15:56:36 +0100

    Updated LSP to enable lsp haskell.

---
+++ elpa/anaconda-mode-20250310.1512/anaconda-mode-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "anaconda-mode" "20250310.1512"
+  "Code navigation, documentation lookup and completion for Python."
+  '((emacs    "25.1")
+    (pythonic "0.1.0")
+    (dash     "2.6.0")
+    (s        "1.9")
+    (f        "0.16.2"))
+  :url "https://github.com/proofit404/anaconda-mode"
+  :commit "28b3e0088ac7113390aa006bf277c8aa14e561a2"
+  :revdesc "28b3e0088ac7"
+  :keywords '("convenience" "anaconda")
+  :authors '(("Artem Malyshev" . "proofit404@gmail.com"))
+  :maintainers '(("Artem Malyshev" . "proofit404@gmail.com")))
