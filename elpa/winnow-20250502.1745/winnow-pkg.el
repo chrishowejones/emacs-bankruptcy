6e9b389db1dc00b57e19e994c22a28fba136f3aa
Chris Howe-Jones <chris.howejones@gmail.com>
2026-03-19 11:25:09 +0000

    Update packages.

---
+++ elpa/winnow-20250502.1745/winnow-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "winnow" "20250502.1745"
+  "Winnow ag/grep results by matching/excluding lines."
+  '((emacs "24"))
+  :url "https://github.com/dgtized/winnow.el"
+  :commit "858e74314c06c060596d6e6119471deef759be4d"
+  :revdesc "858e74314c06"
+  :keywords '("matching")
+  :authors '(("Charles L.G. Comstock" . "dgtized@gmail.com"))
+  :maintainers '(("Charles L.G. Comstock" . "dgtized@gmail.com")))
