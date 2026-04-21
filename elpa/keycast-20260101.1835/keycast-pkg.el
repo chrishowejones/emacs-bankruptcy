c9cebec8fba5e5ced52d24f890170a994b68ae85
Chris Howe-Jones <chris.howejones@gmail.com>
2026-04-21 16:42:56 +0100

    Update forge, keycast, transient and vscode-dark theme.

---
+++ elpa/keycast-20260101.1835/keycast-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "keycast" "20260101.1835"
+  "Show current command and its binding."
+  '((emacs  "28.1")
+    (compat "30.1"))
+  :url "https://github.com/tarsius/keycast"
+  :commit "b831e380c4deb1d51ce5db0a965b96427aec52e4"
+  :revdesc "b831e380c4de"
+  :keywords '("multimedia")
+  :authors '(("Jonas Bernoulli" . "emacs.keycast@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.keycast@jonas.bernoulli.dev")))
---
+++ elpa/vs-dark-theme-20260217.1623/vs-dark-theme-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "vs-dark-theme" "20260217.1623"
+  "Visual Studio IDE dark theme."
+  '((emacs "24.1"))
+  :url "https://github.com/emacs-vs/vs-dark-theme"
+  :commit "ebc176a83808772746c55c91420a95b28b84c869"
+  :revdesc "ebc176a83808"
+  :keywords '("faces"))
