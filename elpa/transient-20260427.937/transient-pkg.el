97c6b0eec99105ee54622ceb797bbf9a6fe1db7c
Chris Howe-Jones <chris.howejones@gmail.com>
2026-05-13 14:39:21 +0100

    Update magit

---
+++ elpa/transient-20260427.937/transient-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20260427.937"
+  "Transient commands."
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.2")
+    (seq      "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "59b0ea8d31571e9d6edaaed4b0b6af4bef7d2793"
+  :revdesc "59b0ea8d3157"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
---
+++ elpa/vscode-dark-plus-theme-20260420.8/vscode-dark-plus-theme-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "vscode-dark-plus-theme" "20260420.8"
+  "Default Visual Studio Code Dark+ theme."
+  ()
+  :url "https://github.com/ianpan870102/vscode-dark-plus-emacs-theme"
+  :commit "082b012d203fed17d54b4a3b7437db545f15ddbb"
+  :revdesc "082b012d203f")
